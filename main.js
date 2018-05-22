7294325755ad0f64bbb5efb0d9af75c83801f9da
sdana <seth.dana@gmail.com>
2018-05-22 13:34:32 -0500

    Added styles to headline DOM element

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ function addMissionStatement(){
     let missionStatementTextP = document.createElement("p");
     let missionStatementText = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum sodales neque sed semper. Fusce molestie magna sit amet lacus vulputate, a molestie mauris aliquet. Etiam ut felis in nunc tincidunt varius vel nec orci. Donec velit metus, pellentesque consequat sollicitudin et, consectetur sit amet nisl. Nullam vel tristique purus, at faucibus dolor. Nulla eu auctor lorem. Proin quis nibh nec elit tempus condimentum ut sit amet risus. Praesent ac nibh viverra, aliquet orci sed, fermentum dolor. Nam rhoncus sagittis tellus eu suscipit.");
     
-    missionStatementHeader.style.fontSize = "xx-large";
+    missionStatementHeader.setAttribute("class", "missionStatementHeader");
+    // missionStatementHeader.style.fontSize = "x-large";
     
     // missionStatementSection.appendChild(missionStatementHeader);
     // missionStatementHeader.appendChild(missionStatementHeaderText);
